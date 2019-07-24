c2c278be977378900796f2b00d8844cee230e55d
Sreenath S <sreenathsirobhushanam@gmail.com>
2019-07-25 00:10:12 +0530

    uncommented all methods

--- FileHandling/index.js
+++ FileHandling/index.js
@@ -1,8 +1,8 @@
 const coreNode = require('./coreNode');
 
-// coreNode.readFileSynschronously();
-// coreNode.readFileAsyncronously();
-// coreNode.readFileFromDirectorySyncronously();
-// coreNode.readFileFromDirectoryAsyncronously();
-// coreNode.writeFileSynchronously();
+coreNode.readFileSynschronously();
+coreNode.readFileAsyncronously();
+coreNode.readFileFromDirectorySyncronously();
+coreNode.readFileFromDirectoryAsyncronously();
+coreNode.writeFileSynchronously();
 coreNode.writeFileAsynchronously();
